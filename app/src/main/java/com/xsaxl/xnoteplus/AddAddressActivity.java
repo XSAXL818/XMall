0ac9400a48f0f6314d89a69334c308a45bff6669
Xsaxl <752719591@qq.com>
2023-12-08 16:15:23 +0800

    收货地址的设置和删除完成

---
+++ app/src/main/java/com/xsaxl/xnoteplus/AddAddressActivity.java
@@ -0,0 +1,14 @@
+package com.xsaxl.xnoteplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_address);
+    }
+}
